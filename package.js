7f1448a4a7ca457ff156b65f62f3339a2e28b42d
Beerses <bart3kdracz@gmail.com>
2026-06-01 17:08:16 +0200

    Create package.js

---
+++ package.js
@@ -0,0 +1,14 @@
+{
+  "name": "roblox-rank-bot",
+  "version": "1.0.0",
+  "description": "Roblox ranking backend server",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.19.2",
+    "better-sqlite3": "^11.0.0",
+    "dotenv": "^16.4.5"
+  }
+}
